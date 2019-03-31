65fcc53f0bc21fdd1352b1be583a67053097ba03
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-30 20:40:21 -0500

    Set style for TodoItem using getStyle()

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// inline styling requires {{}}, unless you use a variable. Then it's {}
 export class TodoItem extends React.Component {
+	getStyle = () => {
+			return {
+				background: '#f4f4f4',
+				padding: '10px',
+				borderBottom: '1px #ccc dotted',
+				textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+			}
+	}
+
 	render() {
 		return (
-			<div>
+			<div style={this.getStyle()}>
 				<p>{ this.props.todo.title }</p>
 			</div>
 		)
@@ -15,5 +25,6 @@ export class TodoItem extends React.Component {
 TodoItem.propTypes = {
 	todo: PropTypes.object.isRequired
 }
+
 
 export default TodoItem;
